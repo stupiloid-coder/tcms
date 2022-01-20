f13f7ae276b054511cca346349f0fd10133aa3c9
stupiloid-coder <riteshkumar.dp@somaiya.edu>
2022-01-20 09:41:03 +0530

    android app

---
+++ app/src/main/java/com/example/tcms/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.tcms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.fun_2048_id);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://tcms-kjsieit.herokuapp.com/");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+}
